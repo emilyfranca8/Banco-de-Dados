8e28a7e803011c4c552e21caa73e276614ce9c89
emilyfranca8 <emily.franca.ribeiro@escola.pr.gov.br>
2025-05-29 11:00:17 -0300

    aula29demaio

--- primeiro.sql
+++ primeiro.sql
@@ -1,1 +1,13 @@
 CREATE DATABASE vendas_sucos
+
+CREATE DATABASE vendas_sucos2 DEFAULT CHARACTER SET UTF8; 
+
+CREATE DATABASE IF NOT exists vendas_sucos2;
+
+DROP DATABASE IF EXISTS vendas_sucos2;
+
+USE vendas_sucos;
+
+CREATE TABLE produtos
+
+
