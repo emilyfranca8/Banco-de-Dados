8df84518a79f5b75dfc825ec3353c4eb2dc53cc7
emilyfranca8 <emily.franca.ribeiro@escola.pr.gov.br>
2025-07-29 14:44:05 -0300

    29-07

--- primeiro.sql
+++ primeiro.sql
@@ -147,4 +147,96 @@ SET A.FERIAS = B.DE_FERIAS;
 
 INSERT INTO PRODUTOS (CODIGO,DESCRITOR,SABOR,TAMANHO,EMABALAGEM,PRECO_LISTA)
    VALUES ('1001001','Sabor dos Alpes 700 ml - Manga','Manga','700 ml','Garrafa',7.50),
-   1001000'Sabor dos Alpes 700 ml - Melão','Melão',
+   ('1001000','Sabor dos Alpes 700 ml - Melão','Melão','700 ml','Garrafa',7.50),
+   ('1001002','Sabor dos Alpes 700 ml - Graviola','Graviola','700 ml','Garrafa',7.50),
+   ('1001003','Sabor dos Alpes 700 ml - Tangerina','Tangerina','700 ml','Garrafa',7.50),
+   ('1001004','Sabor dos Alpes 700 ml - Abacate','Abacate','700 ml','Garrafa',7.50),
+   ('1001005','Sabor dos Alpes 700 ml - Açai','Açai','700 ml','Garrafa',7.50),
+   ('1001006','Sabor dos Alpes 1 Litro - Manga','Manga','1 Litro','Garrafa',7.50),
+   ('1001007','Sabor dos Alpes 1 Litro - Melão','Melão','1 Litro','Garrafa',7.50),
+   ('1001008','Sabor dos Alpes 1 Litro - Graviola','Graviola','1 Litro','Garrafa',7.50),
+   ('1001009','Sabor dos Alpes 1 Litro - Tangerina','Tangerina','1 Litro','Garrafa',7.50),
+   ('1001010','Sabor dos Alpes 1 Litro - Abacate','Abacate','1 Litro','Garrafa',7.50),
+   ('1001011','Sabor dos Alpes 1 Litro - Açai','Açai','1 Litro','Garrafa',7.50);
+
+   SELECT * FROM PRODUTOS WHERE SUBSTRING(DESCRITOR,1,15) = 'Sabor dos Alpes';
+
+   DELETE FROM PRODUTOS WHERE CODIGO = '1001000';
+
+   DELETE FROM PRODUTOS WHERE TAMANHO = '1 Litro'
+   AND SUBSTRING(DESCRITOR,1,15) = 'Sabor dos Alpes';
+
+   SELECT CODIGO_DO_PRODUTO FROM SUCOS_VENDAS.TABELA_DE_PRODUTOS;
+
+   SELECT CODIGO FROM PRODUTOS WHERE 
+   CODIGO NOT IN (SELECT CODIGO_DO_PRODUTO FROM SUCOS_VENDAS.TABELA_DE_PRODUTOS);
+
+   DELETE FROM PRODUTOS WHERE 
+   CODIGO NOT IN (SELECT CODIGO_DO_PRODUTO FROM SUCOS_VENDAS.TABELA_DE_PRODUTOS);
+
+CREATE TABLE 'produtos2' (
+   'CODIGO' varchar(10) NOT NULL,
+   'DESCRITOR' varchar(100) DEFAULT NULL,
+   'SABOR' varchar(50) DEFAULT NULL,
+   'TAMANHO' varchar(50) DEFAULT NULL,
+   'EMBALAGEM' varchar(50) DEFAULT NULL,
+   'PRECO_LISTA' float DEFAULT NULL,
+   PRIMARY KEY ('CODIGO')
+) ;
+
+SELECT * FROM produtos2;
+
+INSERT INTO produto2
+SELECT * FROM produto;
+
+UPDATE produto2 SET preco_lista = 8;
+
+DELETE FROM produtos2;
+
+START TRANSACTION;
+
+SELECT * FROM VENDEDORES;
+
+UPDATE VENDEDORES SET COMISSAO = COMISSAO * 1.15;
+
+ROLLBACK;
+
+COMMIT;
+
+START TRANSACTION;
+
+INSERT INTO 'vendas_sucos'.'vendedores'
+('MATRICULA',
+'NOME',
+'BAIRRO',
+'COMISSAO',
+'DATA_ADMISSAO',
+'FERIAS')
+VALUES
+('999999',
+'JOAO DA SILVA',
+'Icaraí',
+0.08,
+'2012-01-15',
+0);
+
+UPDATE VENDEDORES SET COMISSAO = COMISSAO * 1.15;
+
+INSERT INTO 'vendas_sucos'.'vendedores'
+('MATRICULA',
+'NOME',
+'BAIRRO',
+'COMISSAO',
+'DATA_ADMISSAO',
+'FERIAS')
+VALUES
+('999998',
+'JOAO DA SILVA2',
+'Icaraí',
+0.08,
+'2012-01-15',
+0);
+
+ROLLBACK;
+
+COMMIT;
